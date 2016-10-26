14c59cd8ea04a8e220482d2f0c635bc7b6bbaf48
Amit Doshi <amit.doshi@synechron.com>
2016-10-26 12:40:11 +0530

    Updated site with testimonial and works

---
+++ old/assets/js/main.js
@@ -0,0 +1,18 @@
+;(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+			
+			$('.menu-items').on('click', function(){
+				
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
